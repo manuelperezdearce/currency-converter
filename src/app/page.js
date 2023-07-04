3a4ce382414634dd57d09c3b130f59acec44681f
manuelperezdearce <manuel.perezdearce@gmail.com>
2023-07-04 00:42:18 -0400

    F/manuel: added indicator api rest for each

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
 
   const [data, setData] = useState({})
   const [dataArray, setDataArray] = useState([])
-  const [indicator, setIndicator] = useState([])
+  const [indicator, setIndicator] = useState('')
   let counter = 0
 
   const getAPI = async () => {
@@ -15,6 +15,19 @@ export default function Home() {
     const API_DATA = await res.json()
     setData(API_DATA)
   }
+
+  const getIndicatorValues = async () => {
+    const API_URL = `https://mindicador.cl/api/${indicator}`
+    const res = await fetch(API_URL)
+    const API_DATA = await res.json()
+    console.log(API_DATA)
+  }
+
+  useEffect(() => {
+    getIndicatorValues()
+  }, [indicator])
+
+
 
   useEffect(() => {
     getAPI()
@@ -35,10 +48,10 @@ export default function Home() {
         <div className="w-full bg-slate-400 p-4">
           <form className="flex flex-col gap-5 justify-center">
             <h2>Seleccione un indicador</h2>
-            <select>
+            <select onChange={(e) => setIndicator(e.target.value)}>
               {data && dataArray.map((item) => {
                 counter++
-                return counter > 3 ? <option key={counter} value={'0'}>{item.nombre}</option> : null
+                return counter > 3 ? <option key={counter} value={item.codigo}>{item.nombre}</option> : null
               })}
             </select>
           </form>
